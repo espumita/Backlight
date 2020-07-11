bcc5fc8019abb493d1aada66bf204fa878fc629b
Espumita <davidfalconnaranjo@gmail.com>
2020-07-11 11:04:31 +0100

    Ignore spaces

--- BackLight/BackLight/BackLightConfiguration.cs
+++ BackLight/BackLight/BackLightConfiguration.cs
@@ -1,13 +1,7 @@
-using System;
-using System.IO;
-using System.Reflection;
-
 namespace BackLight {
     public class BackLightConfiguration {
 
         public string RoutePrefix { get; set; } = "backlight";
-
-
 
         public string IndexHtmlDocumentTitle { get; set; } = "BackLight";
     }
--- BackLight/BackLight/BackLightMiddleware.cs
+++ BackLight/BackLight/BackLightMiddleware.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
