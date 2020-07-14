4103d298751644a34e4b90a8d5845fd9eb53de17
Espumita <davidfalconnaranjo@gmail.com>
2020-07-14 21:44:55 +0100

    Map api enpoints

--- BackLight/BackLight/BackLightBuilderExtensions.cs
+++ BackLight/BackLight/BackLightBuilderExtensions.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Net;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
@@ -14,8 +16,33 @@ namespace Backlight {
             }
 
             applicationBuilder.UseMiddleware<BacklightMiddleware>(configuration);
-
+            applicationBuilder.Map($"/{configuration.RoutePrefix}/api", MapApi);
             return applicationBuilder;
+        }
+
+        private static void MapApi(IApplicationBuilder applicationBuilder) {
+            applicationBuilder.Run(async context => {
+                if (context.Request.Method == HttpMethods.Put) {
+                    context.Response.StatusCode = (int)HttpStatusCode.OK;
+                    await context.Response.WriteAsync("Create");
+                }
+                if (context.Request.Method == HttpMethods.Get) {
+                    context.Response.StatusCode = (int)HttpStatusCode.OK;
+                    await context.Response.WriteAsync("Read");
+                }
+                if (context.Request.Method == HttpMethods.Post) {
+                    context.Response.StatusCode = (int)HttpStatusCode.OK;
+                    await context.Response.WriteAsync("Update");
+                }
+                if (context.Request.Method == HttpMethods.Delete) {
+                    context.Response.StatusCode = (int)HttpStatusCode.OK;
+                    await context.Response.WriteAsync("Delete");
+                }
+
+                context.Response.StatusCode = (int) HttpStatusCode.MethodNotAllowed;
+                await context.Response.WriteAsync("Bad Request");
+
+            });
         }
     }
 }
