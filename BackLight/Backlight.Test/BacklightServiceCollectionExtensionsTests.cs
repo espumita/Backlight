1d500d9f2633f6d9c1ee379066ff0f4fc7e30758
Espumita <davidfalconnaranjo@gmail.com>
2020-07-19 21:52:12 +0100

    Green: be_configued_with_a_create_provider

--- BackLight/Backlight.Test/BacklightServiceCollectionExtensionsTests.cs
+++ BackLight/Backlight.Test/BacklightServiceCollectionExtensionsTests.cs
@@ -1,7 +1,10 @@
 ﻿using System.Linq;
+using System.Text.Json;
+using Backlight.Providers;
 using Backlight.Services;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace Backlight.Test {
@@ -27,6 +30,32 @@ namespace Backlight.Test {
             configuration.ReadProvidersDelegates.Should().BeEmpty();
             configuration.UpdateProvidersDelegates.Should().BeEmpty();
             configuration.DeleteProvidersDelegates.Should().BeEmpty();
+        }
+
+        [Test]
+        public void be_configued_with_a_create_provider() {
+            var createProvider = Substitute.For<CreateProvider>();
+            collection.AddBacklight(configuration => {
+                configuration.For<UserEntity>()
+                    .AddCreate(createProvider);
+            });
+
+            var serviceDescriptor = collection.Single();
+            serviceDescriptor.Lifetime.Should().Be(ServiceLifetime.Singleton);
+            serviceDescriptor.ServiceType.Should().Be(typeof(BacklightProvidersService));
+            var backlightProvidersService = (BacklightProvidersService)serviceDescriptor.ImplementationInstance;
+            var configuration = backlightProvidersService.Configuration;
+            var keyValuePair = configuration.Providers.Single();
+            keyValuePair.Key.Should().Be<UserEntity>();
+            keyValuePair.Value.CanCreate().Should().BeTrue();
+            var valuePair = configuration.CreateProvidersDelegates.Single();
+            valuePair.Key.Should().Be<UserEntity>();
+            var userEntity = new UserEntity{ Name = "aName", Age = 23};
+            valuePair.Value(JsonSerializer.Serialize(userEntity));
+            createProvider.Received().Create(userEntity);
+            //configuration.ReadProvidersDelegates.Should().BeEmpty();
+            //configuration.UpdateProvidersDelegates.Should().BeEmpty();
+            //configuration.DeleteProvidersDelegates.Should().BeEmpty();
         }
     }
 }
