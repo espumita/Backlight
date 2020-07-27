0474b1bc9467f0f17fa2d27cbddc152f0a7069c1
Espumita <davidfalconnaranjo@gmail.com>
2020-07-27 20:57:37 +0100

    Refactor: extract AnEntityPayloadWith

--- BackLight/Backlight.Test/Services/BacklightServiceTests.cs
+++ BackLight/Backlight.Test/Services/BacklightServiceTests.cs
@@ -9,7 +9,7 @@ using NUnit.Framework;
 namespace Backlight.Test.Services {
     public class BacklightServiceTests {
         private string AEntityName = typeof(UserEntity).FullName;
-        private EntitySerializer entitySerializer;
+        private EntitySerializer serializer;
         private ServiceOptions options;
         private const int AnAge = 12;
         private const string AName = "aName";
@@ -19,20 +19,17 @@ namespace Backlight.Test.Services {
 
         [SetUp]
         public void SetUp() {
-            entitySerializer = Substitute.For<EntitySerializer>();
-            options = new ServiceOptions(entitySerializer);
+            serializer = Substitute.For<EntitySerializer>();
+            options = new ServiceOptions(serializer);
         }
 
         [Test]
         public async Task use_provider_on_create() {
             var provider = Substitute.For<CreateProvider>();
             options.For<UserEntity>().AddCreate(provider);
-            entitySerializer.Deserialize<UserEntity>(ASerializedEntity).Returns(aUserEntity);
-            var anEntityPayload = new EntityPayload {
-                TypeName = AEntityName,
-                Value = ASerializedEntity
-            };
-
+            serializer.Deserialize<UserEntity>(ASerializedEntity).Returns(aUserEntity);
+            var anEntityPayload = AnEntityPayloadWith(AEntityName, ASerializedEntity);
+            
             await ServiceWith(options).Create(anEntityPayload);
 
             await provider.Received().Create(aUserEntity);
@@ -42,13 +39,10 @@ namespace Backlight.Test.Services {
         public async Task use_provider_on_read() {
             var provider = Substitute.For<ReadProvider>();
             options.For<UserEntity>().AddRead(provider);
-            var anEntityPayload = new EntityPayload {
-                TypeName = AEntityName,
-                Value = AnEntityId
-            };
             provider.Read<UserEntity>(AnEntityId).Returns(aUserEntity);
-            entitySerializer.Serialize(aUserEntity).Returns(ASerializedEntity);
-
+            serializer.Serialize(aUserEntity).Returns(ASerializedEntity);
+            var anEntityPayload = AnEntityPayloadWith(AEntityName, AnEntityId);
+            
             var readedEntity = await ServiceWith(options).Read(anEntityPayload);
 
             readedEntity.Should().Be(ASerializedEntity);
@@ -58,11 +52,8 @@ namespace Backlight.Test.Services {
         public async Task use_provider_on_update() {
             var provider = Substitute.For<UpdateProvider>();
             options.For<UserEntity>().AddUpdate(provider);
-            entitySerializer.Deserialize<UserEntity>(ASerializedEntity).Returns(aUserEntity);
-            var anEntityPayload = new EntityPayload {
-                TypeName = AEntityName,
-                Value = ASerializedEntity
-            };
+            serializer.Deserialize<UserEntity>(ASerializedEntity).Returns(aUserEntity);
+            var anEntityPayload = AnEntityPayloadWith(AEntityName, ASerializedEntity);
 
             await ServiceWith(options).Update(anEntityPayload);
 
@@ -73,17 +64,22 @@ namespace Backlight.Test.Services {
         public async Task use_provider_on_delete() {
             var provider = Substitute.For<DeleteProvider>();
             options.For<UserEntity>().AddDelete(provider);
-            var anEntityPayload = new EntityPayload {
-                TypeName = AEntityName,
-                Value = AnEntityId
-            };
+            var anEntityPayload = AnEntityPayloadWith(AEntityName, AnEntityId);
 
             await ServiceWith(options).Delete(anEntityPayload);
 
             await provider.Received().Delete<UserEntity>(AnEntityId);
         }
+
         private BacklightService ServiceWith(ServiceOptions options) {
             return new BacklightService(options);
+        }
+
+        private EntityPayload AnEntityPayloadWith(string typeName, string value) {
+            return new EntityPayload {
+                TypeName = typeName,
+                Value = value
+            };
         }
 
     }
