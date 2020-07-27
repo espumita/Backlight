72b84a0a6f3e2907d9ef96b466fc0be5b9440214
Espumita <davidfalconnaranjo@gmail.com>
2020-07-27 20:48:04 +0100

    Refactor: extract methods

--- BackLight/Backlight.Test/Services/BacklightServiceTests.cs
+++ BackLight/Backlight.Test/Services/BacklightServiceTests.cs
@@ -9,87 +9,81 @@ using NUnit.Framework;
 namespace Backlight.Test.Services {
     public class BacklightServiceTests {
         private string AEntityName = typeof(UserEntity).FullName;
+        private EntitySerializer entitySerializer;
+        private ServiceOptions options;
+        private const int AnAge = 12;
+        private const string AName = "aName";
+        private UserEntity aUserEntity = new UserEntity { Age = AnAge, Name = AName };
         private const string ASerializedEntity = "aSerializedEntity";
         private const string AnEntityId = "anEntityId";
-        private const string AName = "aName";
-        private const int AnAge = 12;
 
         [SetUp]
         public void SetUp() {
+            entitySerializer = Substitute.For<EntitySerializer>();
+            options = new ServiceOptions(entitySerializer);
         }
 
         [Test]
         public async Task use_provider_on_create() {
-            var createProvider = Substitute.For<CreateProvider>();
-            var entitySerializer = Substitute.For<EntitySerializer>();
-            var serviceOptions = new ServiceOptions(entitySerializer);
-            serviceOptions.For<UserEntity>().AddCreate(createProvider);
-            var backlightService = new BacklightService(serviceOptions);
-            var aUserEntity = new UserEntity { Age = AnAge, Name = AName };
+            var provider = Substitute.For<CreateProvider>();
+            options.For<UserEntity>().AddCreate(provider);
             entitySerializer.Deserialize<UserEntity>(ASerializedEntity).Returns(aUserEntity);
             var anEntityPayload = new EntityPayload {
                 TypeName = AEntityName,
                 Value = ASerializedEntity
             };
 
-            await backlightService.Create(anEntityPayload);
+            await ServiceWith(options).Create(anEntityPayload);
 
-            await createProvider.Received().Create(aUserEntity);
+            await provider.Received().Create(aUserEntity);
         }
 
         [Test]
         public async Task use_provider_on_read() {
-            var readProvider = Substitute.For<ReadProvider>();
-            var entitySerializer = Substitute.For<EntitySerializer>();
-            var serviceOptions = new ServiceOptions(entitySerializer);
-            serviceOptions.For<UserEntity>().AddRead(readProvider);
-            var backlightService = new BacklightService(serviceOptions);
-            var aUserEntity = new UserEntity { Age = AnAge, Name = AName };
+            var provider = Substitute.For<ReadProvider>();
+            options.For<UserEntity>().AddRead(provider);
             var anEntityPayload = new EntityPayload {
                 TypeName = AEntityName,
                 Value = AnEntityId
             };
-            readProvider.Read<UserEntity>(AnEntityId).Returns(aUserEntity);
+            provider.Read<UserEntity>(AnEntityId).Returns(aUserEntity);
             entitySerializer.Serialize(aUserEntity).Returns(ASerializedEntity);
 
-            var readedEntity = await backlightService.Read(anEntityPayload);
+            var readedEntity = await ServiceWith(options).Read(anEntityPayload);
+
             readedEntity.Should().Be(ASerializedEntity);
         }
 
         [Test]
         public async Task use_provider_on_update() {
-            var updateProvider = Substitute.For<UpdateProvider>();
-            var entitySerializer = Substitute.For<EntitySerializer>();
-            var serviceOptions = new ServiceOptions(entitySerializer);
-            serviceOptions.For<UserEntity>().AddUpdate(updateProvider);
-            var backlightService = new BacklightService(serviceOptions);
-            var aUserEntity = new UserEntity { Age = AnAge, Name = AName };
+            var provider = Substitute.For<UpdateProvider>();
+            options.For<UserEntity>().AddUpdate(provider);
             entitySerializer.Deserialize<UserEntity>(ASerializedEntity).Returns(aUserEntity);
             var anEntityPayload = new EntityPayload {
                 TypeName = AEntityName,
                 Value = ASerializedEntity
             };
 
-            await backlightService.Update(anEntityPayload);
+            await ServiceWith(options).Update(anEntityPayload);
 
-            await updateProvider.Received().Update("TODOEntityId", aUserEntity);
+            await provider.Received().Update("TODOEntityId", aUserEntity);
         }
 
         [Test]
         public async Task use_provider_on_delete() {
-            var deleteProvider = Substitute.For<DeleteProvider>();
-            var entitySerializer = Substitute.For<EntitySerializer>();
-            var serviceOptions = new ServiceOptions(entitySerializer);
-            serviceOptions.For<UserEntity>().AddDelete(deleteProvider);
-            var backlightService = new BacklightService(serviceOptions);
+            var provider = Substitute.For<DeleteProvider>();
+            options.For<UserEntity>().AddDelete(provider);
             var anEntityPayload = new EntityPayload {
                 TypeName = AEntityName,
                 Value = AnEntityId
             };
 
-            await backlightService.Delete(anEntityPayload);
+            await ServiceWith(options).Delete(anEntityPayload);
 
-            await deleteProvider.Received().Delete<UserEntity>(AnEntityId);
+            await provider.Received().Delete<UserEntity>(AnEntityId);
+        }
+        private BacklightService ServiceWith(ServiceOptions options) {
+            return new BacklightService(options);
         }
 
     }
