8a237688170856c82beb16f70fa189166e096cd8
Espumita <davidfalconnaranjo@gmail.com>
2020-07-31 00:26:55 +0100

    Remove json property name

--- BackLight/BackLight.Sample.Web.Api/Entities/ExampleEntity.cs
+++ BackLight/BackLight.Sample.Web.Api/Entities/ExampleEntity.cs
@@ -1,9 +1,7 @@
-using System.Text.Json.Serialization;
 
 namespace Backlight.Sample.Web.Api.Entities {
     public class ExampleEntity : BacklightEntity {
 
-        [JsonPropertyName("asd")]
         public string Name { get; set; } = "EntityName";
 
     }
