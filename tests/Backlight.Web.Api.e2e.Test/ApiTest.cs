319a46005ab95841dca1365fe991a31f24ada312
Espumita <davidfalconnaranjo@gmail.com>
2020-08-05 20:52:01 +0100

    Reorder project and create e2e tests

---
+++ tests/Backlight.Web.Api.e2e.Test/ApiTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Backlight.Web.Api.e2e.Test {
+    public class ApiTest {
+        private HttpClient client;
+
+        [SetUp]
+        public void SetUp() {
+            client = TestFixture.serverClient;
+        }
+
+
+        [Test]
+        public async Task read() {
+            var response = await client.GetAsync("/back/api/entity/2");
+
+        }
+    }
+}
---
+++ tests/Backlight.Web.Api.e2e.Test/TestFixture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using Backlight.Sample.Web.Api;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using NUnit.Framework;
+
+namespace Backlight.Web.Api.e2e.Test {
+
+    [SetUpFixture]
+    public class TestFixture {
+
+        public static HttpClient serverClient;
+
+
+        [OneTimeSetUp]
+        public void SetUp() {
+            StartTestApiServer();
+        }
+
+        private void StartTestApiServer() {
+            const string testServerUrl = "http://localhost:50351";
+            var webHostBuilder = new WebHostBuilder()
+                .UseUrls(testServerUrl)
+                .UseStartup<Startup>();
+            var testServer = new TestServer(webHostBuilder);
+            serverClient = testServer.CreateClient();
+            serverClient.BaseAddress = new Uri(testServerUrl);
+        }
+
+    }
+}
